ef28b4683283f87f4a3b680b0f6b68a0cc363be7
Chris Swagler <76789839+chrisswagler@users.noreply.github.com>
2021-03-26 15:01:51 -0400

    Update IR emission/receiver

--- FYELIC-Examples/IR_MODULE/IR_Emission/IR_Emission.ino
+++ FYELIC-Examples/IR_MODULE/IR_Emission/IR_Emission.ino
@@ -1,19 +1,35 @@
-/******************************************
- *Website: www.elegoo.com
- * 
- *Time:2017.12.12
- *
- ******************************************/
+/*
+   modified 26 March 2021
+   by Chris Swagler
+   purpose: to use the IR emitter to send out different hexadecimal codes
+   
+   Note: THE IRREMOTE LIBRARY AT THE TIME OF THIS SKETCH WAS IN THE PROCESS OF BEING UPDATED.
+         Accordingly, this sketch might not work for the most recent version of the libary.
+         Check the github page for the IRremote https://github.com/Arduino-IRremote/Arduino-IRremote
+         to find the latest version and any updates necessary to your code.
+
+         For this sketch, the most recent version of the library was not functional and had bugs,
+         so the version used for this was Version 2.2.3. To get this version, go to
+         Tools > Manage Libraries > search IRremote > change the version and install library
+
+         To test if this is working, an IR receiver should be used with the emitter. I personally connected
+         another redboard and uploaded the IR_Receiver sketch (found in the FYELIC library) to test
+         that it was actually receiving these signals. 
+*/
+
 //An IR LED must be connected to Arduino PWM pin 3.
- #include <IRremote.h>
-  IRsend irsend;
- void setup()
- {}
- void loop() 
- {
+
+#include <IRremote.h>
+IRsend irsend;
+void setup() {}
+
+void loop() {
   irsend.sendRC5(0x0, 8); //send 0x0 code (8 bits)
-     delay(200);
-  irsend.sendRC5(0x1, 8); 
-     delay(200); 
-  }
- 
+  delay(200);
+  irsend.sendRC5(0x1, 8); //send 0x1 code (8 bits)
+  delay(200);
+  irsend.sendRC5(0xFF, 8); //send 0xFF code (8 bits)
+  delay(200);
+  irsend.sendRC5(0x5F, 8); //send 0x5F code (8 bits)
+  delay(200);
+}
--- FYELIC-Examples/IR_MODULE/IR_Receiver/IR_Receiver.ino
+++ FYELIC-Examples/IR_MODULE/IR_Receiver/IR_Receiver.ino
@@ -1,35 +1,37 @@
-/******************************************
- *Website: www.elegoo.com
- * 
- *Time:2017.12.12
- *
- ******************************************/
- #include <IRremote.h>
- #define RECV_PIN  11        //Infrared signal receiving pin
- #define LED       13        //define LED pin
- IRrecv irrecv(RECV_PIN);
- decode_results results;
- void setup()
- { 
-   pinMode(LED, OUTPUT); //initialize LED as an output
-   Serial.begin(9600);
-   irrecv.enableIRIn(); // Start the receiver
- }
-  void loop() 
- {
-  if (irrecv.decode(&results)) 
-   { 
-     int state;
-       if ( results.value == 1 )  
-       {    
-         state = HIGH;
-       }
-       else
-       {
-        state = LOW;
-       }
-    digitalWrite( LED, state );     
-    Serial.println(results.value);
+/*
+   modified 26 March 2021
+   by Chris Swagler
+   purpose: to use the IR receiver to receive signals and output their hexadecimal value
+
+   Note: THE IRREMOTE LIBRARY AT THE TIME OF THIS SKETCH WAS IN THE PROCESS OF BEING UPDATED.
+         Accordingly, this sketch will not work for the most recent version of the libary.
+         Check the github page for the IRremote https://github.com/Arduino-IRremote/Arduino-IRremote
+         to find the latest version and any updates necessary to your code.
+
+         For this sketch, the most recent version of the library was not functional and had bugs,
+         so the version used for this was Version 2.2.3. To get this version, go to
+         Tools > Manage Libraries > search IRremote > change the version and install library
+
+         Functional edits for your version - Use the serial monitor to determine the value of the
+         incoming IR signal in hexadecimal format. You can then use if statements to respond to 
+         particular signals using _results.value_. They aren't included here since there are a wide
+         variety of remotes/ IR emitters.
+*/
+#include <IRremote.h>
+int RECV_PIN = 11;       //Infrared signal receiving pin
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup() {
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop() {
+  
+  //if the recevier receives a signal, decode the results
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX); //print the results in hexadecimal format
     irrecv.resume();        // prepare to receive the next value
-   }
- }
+  }
+}
